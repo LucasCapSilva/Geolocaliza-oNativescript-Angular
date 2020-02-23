44a964ab4e6479589edad2c29eb648dc6df7fb01
unknown <lcapelotto@gmail.com>
2020-02-22 21:19:18 -0300

    conect api gps

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,84 @@
+import { Component, OnInit } from "@angular/core";
+import * as geolocation from "nativescript-geolocation";
+import { Accuracy } from "tns-core-modules/ui/enums";
+
+@Component({
+    selector: "Home",
+    templateUrl: "./home.component.html"
+})
+export class HomeComponent implements OnInit {
+
+    locations = [];
+    watchIds = []
+
+    constructor() {
+        // Use the component constructor to inject providers.
+    }
+
+    ngOnInit(): void {
+        // Init your component properties here.
+    }
+
+    public buttonGetLocationTap() {
+        geolocation.isEnabled().then(function (isEnabled) {
+            if (!isEnabled) {
+                geolocation.enableLocationRequest(true, true).then(() => {
+                    console.log("User Enabled Location Service");
+                }, (e) => {
+                    console.log("Error: " + (e.message || e));
+                }).catch(ex => {
+                    console.log("Unable to Enable Location", ex);
+                });
+            }
+        }, function (e) {
+            console.log("Error: " + (e.message || e));
+        });
+        let that = this;
+        geolocation.getCurrentLocation({
+            desiredAccuracy: Accuracy.high,
+            maximumAge: 5000,
+            timeout: 10000
+        }).then(function (loc) {
+            if (loc) {
+                that.locations.push(loc);
+            }
+        }, function (e) {
+            console.log("Error: " + (e.message || e));
+        });
+    }
+
+    public buttonStartTap() {
+        try {
+            let that = this;
+            this.watchIds.push(geolocation.watchLocation(
+                function (loc) {
+                    if (loc) {
+                        that.locations.push(loc);
+                    }
+                },
+                function (e) {
+                    console.log("Error: " + e.message);
+                },
+                {
+                    desiredAccuracy: Accuracy.high,
+                    updateDistance: 1,
+                    updateTime: 3000,
+                    minimumUpdateTime: 100
+                }));
+        } catch (ex) {
+            console.log("Error: " + ex.message);
+        }
+    }
+
+    public buttonStopTap() {
+        let watchId = this.watchIds.pop();
+        while (watchId != null) {
+            geolocation.clearWatch(watchId);
+            watchId = this.watchIds.pop();
+        }
+    }
+
+    public buttonClearTap() {
+        this.locations.splice(0, this.locations.length);
+    }
+}
